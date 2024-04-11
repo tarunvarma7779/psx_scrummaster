5d3cd1579e2bd1c99b0c2047ed4c2abebc31acd5
Akhil Vudatha <akhilvsvs1999@gmail.com>
2024-04-11 15:59:24 +0530

    CorsOrigin

--- src/main/java/com/posidex/config/CorsConfig.java
+++ src/main/java/com/posidex/config/CorsConfig.java
@@ -1,19 +1,22 @@
 package com.posidex.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
+	@Autowired
+	Environment environment;
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/**").allowedOrigins("http://"+environment.getProperty("cors.origin")+":4200")
 						.allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(true).maxAge(3600);
 			}
 		};
